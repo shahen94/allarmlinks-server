a31b93ae906e3bcfed33c5301ab4190d8b25446e
Armen Matevosyan <aamatevosyan@edu.hse.ru>
2020-11-12 18:23:18 +0400

    Fixed language search

--- src/modules/volunteer/volunteer.model.ts
+++ src/modules/volunteer/volunteer.model.ts
@@ -1,6 +1,6 @@
-import mongoose, { DocumentQuery, Model } from "mongoose";
-import { Document, Schema } from "mongoose";
-import { caseInsExp } from '../../utils/regexp'
+import mongoose, {Document, DocumentQuery, Model, Schema} from "mongoose";
+import {caseInsExp} from '../../utils/regexp'
+
 type Nullable<T> = T | null;
 
 export const STATUS_INITIALIZED = "initialized";
@@ -38,29 +38,30 @@ export interface IVolunteer extends Document {
 const VolunteerSchema: Schema = new mongoose.Schema({
     name: String,
     surname: String,
-    email: { type: String, trim: true, index: true, unique: true, sparse: true },
-    phone: { type: String, trim: true, index: true, unique: true, sparse: true, default: undefined },
-    status: { type: String, default: STATUS_INITIALIZED },
-    birthDate: { type: String, default: null },
-    country: { type: String, default: null },
-    city: { type: String, default: null },
-    address: { type: String, default: null },
-    specialization: { type: String, default: null },
-    currentEmployerName: { type: String, default: null },
-    occupation: { type: String, default: null },
+    email: {type: String, trim: true, index: true, unique: true, sparse: true},
+    phone: {type: String, trim: true, index: true, unique: true, sparse: true, default: undefined},
+    status: {type: String, default: STATUS_INITIALIZED},
+    birthDate: {type: String, default: null},
+    country: {type: String, default: null},
+    city: {type: String, default: null},
+    address: {type: String, default: null},
+    specialization: {type: String, default: null},
+    currentEmployerName: {type: String, default: null},
+    occupation: {type: String, default: null},
     languages: [String],
     hoursPerWeek: {
-        from: { type: Number, default: null },
-        to: { type: Number, default: null },
+        from: {type: Number, default: null},
+        to: {type: Number, default: null},
     },
-    workStatus: { type: String, default: null },
-    facebookProfile: { type: String, default: null },
-    linkedinProfile: { type: String, default: null },
-    twitterProfile: { type: String, default: null },
-    whereToVolunteer: { type: String, default: null },
-    other: { type: String, default: null },
-    note: { type: String, default: null },
+    workStatus: {type: String, default: null},
+    facebookProfile: {type: String, default: null},
+    linkedinProfile: {type: String, default: null},
+    twitterProfile: {type: String, default: null},
+    whereToVolunteer: {type: String, default: null},
+    other: {type: String, default: null},
+    note: {type: String, default: null},
 });
+
 interface IVolunteerModel extends Model<IVolunteer, typeof volunteerQueryHelpers> {
     byFullName(): Promise<any>;
 }
@@ -72,62 +73,61 @@ const volunteerQueryHelpers = {
         if (name2)
             query = {
                 $and: [
-                    { name: caseInsExp(name1) },
-                    { surname: caseInsExp(name2) },
-                    { status: STATUS_FINISHED }
+                    {name: caseInsExp(name1)},
+                    {surname: caseInsExp(name2)},
+                    {status: STATUS_FINISHED}
                 ]
             }
         else
             query = {
                 $and: [
-                    { $or: [{ name: caseInsExp(name1) }, { surname: caseInsExp(name1) }] },
-                    { status: STATUS_FINISHED },
+                    {$or: [{name: caseInsExp(name1)}, {surname: caseInsExp(name1)}]},
+                    {status: STATUS_FINISHED},
                 ]
 
             }
         if (volunteerId) {
             query.$and.push(
                 {
-                    _id: { $gt: volunteerId }
+                    _id: {$gt: volunteerId}
                 }
             )
         }
         return this.where(query)
     },
     byLanguage(this: DocumentQuery<any, IVolunteer>, languages: string, volunteerId: string = '') {
-        const langs = languages.split(' ').map(lang => {
-            return { languages: caseInsExp(lang) }
-        })
+        const langs = languages.split(' ').map(lang => caseInsExp(lang));
+        console.log(langs);
         let query: any = {
             $and: [
                 {
-                    $or: langs
+                    languages: {$in: langs}
                 }
             ]
         }
         if (volunteerId) {
             query.$and.push(
                 {
-                    _id: { $gt: volunteerId }
+                    _id: {$gt: volunteerId}
                 }
             )
         }
         query.$and.push(
-            { status: STATUS_FINISHED }
+            {status: STATUS_FINISHED}
         )
-        return this.where({ $or: query })
+        return this.where(query);
     },
     byEmail(this: DocumentQuery<any, IVolunteer>, email: string, volunteerId: string = '') {
         let query: any = {
             $and: [
-                { email: new RegExp(".*" + email + ".*") },
-                { status: STATUS_FINISHED }
+                {email: new RegExp(".*" + email + ".*")},
+                {status: STATUS_FINISHED}
             ]
         }
         if (volunteerId) {
             query.$and.push(
                 {
-                    _id: { $gt: volunteerId }
+                    _id: {$gt: volunteerId}
                 }
             )
         }
@@ -135,30 +135,29 @@ const volunteerQueryHelpers = {
     },
     byCompanyOccupation(this: DocumentQuery<any, IVolunteer>, companyOccupation: string, volunteerId: string = '') {
         let query: any;
-        query = { $and: [{ $or: [{ currentEmployerName: caseInsExp(companyOccupation) }, { occupation: caseInsExp(companyOccupation) }] }] };
+        query = {$and: [{$or: [{currentEmployerName: caseInsExp(companyOccupation)}, {occupation: caseInsExp(companyOccupation)}]}]};
         if (volunteerId) {
             query.$and.push(
                 {
-                    _id: { $gt: volunteerId }
+                    _id: {$gt: volunteerId}
                 },
-                { status: STATUS_FINISHED }
-
+                {status: STATUS_FINISHED}
             )
         }
         return this.where(query);
     },
     byCountryCity(this: DocumentQuery<any, IVolunteer>, countryCity: string, volunteerId: string = '') {
         let query: any;
-        query = { $and: [{ $or: [{ country: caseInsExp(countryCity) }, { city: caseInsExp(countryCity) }] }] };
+        query = {$and: [{$or: [{country: caseInsExp(countryCity)}, {city: caseInsExp(countryCity)}]}]};
         if (volunteerId) {
             query.$and.push(
                 {
-                    _id: { $gt: volunteerId }
+                    _id: {$gt: volunteerId}
                 }
             )
         }
         query.$and.push(
-            { status: STATUS_FINISHED }
+            {status: STATUS_FINISHED}
         )
         return this.where(query);
     }
